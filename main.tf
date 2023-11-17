fc4b6cc38a17d5e3d580e535c4d52bd1402dbab5
Aramis <aramisoliveira@live.com>
2023-11-16 21:22:19 -0300

    Funcionando

--- main.tf
+++ main.tf
@@ -6,33 +6,57 @@ terraform {
   }
 }
 
-
-
-
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 
-
-
-# Criando topico e assinatura SNS na AWS
-
+# Criando tópico e assinatura SNS na AWS
 resource "aws_sns_topic" "Monitoramento_criacao_remocao_recursos" {
   name = "Monitoramento_atividades"
-  
 }
 
 resource "aws_sns_topic_subscription" "assinatura_email" {
   topic_arn = aws_sns_topic.Monitoramento_criacao_remocao_recursos.arn
-  protocol = "email"
-  endpoint = "aramisoliveira@live.com"
+  protocol  = "email"
+  endpoint  = "aramisoliveira@live.com"
 }
 
+# Criando um EventBridge
+resource "aws_cloudwatch_event_rule" "Monitor_config" {
+  name        = "MinhaRegraEventBridge"
+  description = "Regra para eventos de alteração de Configuração"
 
+  event_pattern = <<PATTERN
+{
+  "source": ["aws.config"],
+  "detail-type": ["Config Configuration Item Change"],
+  "detail": {
+    "messageType": ["ConfigurationItemChangeNotification"],
+    "configurationItem": {
+      "configurationItemStatus": ["ResourceDiscovered", "ResourceDeleted"]
+    }
+  }
+}
+PATTERN
+}
 
+# Definindo como os recursos serao entregues para o destinario
+resource "aws_cloudwatch_event_target" "monitor" {
+  arn  = aws_sns_topic.Monitoramento_criacao_remocao_recursos.arn
+  rule = aws_cloudwatch_event_rule.Monitor_config.id
 
-
-
-# Criando um event bridge
-
+  input_transformer {
+    input_paths = {
+      awsAccountId = "$.detail.configurationItem.awsAccountId",
+      awsRegion    = "$.detail.configurationItem.awsRegion",
+      status       = "$.detail.configurationItem.configurationItemStatus",
+      resourceId   = "$.detail.configurationItem.resourceId",
+      resourceType = "$.detail.configurationItem.resourceType",
+      eventName    = "$.detail.eventName",
+      configurationItemCaptureTime: "$.detail.configurationItem.configurationItemCaptureTime",
+      userName     = "$.detail.userIdentity.userName"
+    }
+    input_template = "\"O recurso <resourceId> foi <status> na hora <configurationItemCaptureTime> na conta <awsAccountId> na região <awsRegion>\""
+  }
+}
 
